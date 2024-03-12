1cf66509d25bedb1bf67a5704f9a316816f69f77
Hihi3020 <ngphtrang520@gmail.com>
2024-03-13 01:46:51 +0700

    implement method for BaseRequest

--- FAP_project/src/java/Controller/lecturer/AttendanceTakingController.java
+++ FAP_project/src/java/Controller/lecturer/AttendanceTakingController.java
@@ -2,34 +2,56 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Controller.lecturer;
 
 import Controller.authentication.BaseRequiredAuthenticationController;
+import dal.LessionDBContext;
 import entity.Account;
+import entity.Attendence;
+import entity.Lession;
+import entity.Student;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /**
  *
  * @author Dell
  */
 public class AttendanceTakingController extends BaseRequiredAuthenticationController {
-   
-
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp, Account account) throws ServletException, IOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int leid = Integer.parseInt(req.getParameter("id"));
+        LessionDBContext db = new LessionDBContext();
+        ArrayList<Student> students = db.getStudentsByLession(leid);
+        ArrayList<Attendence> atts = new ArrayList<>();
+        Lession lession = new Lession();
+        lession.setId(leid);
+        for (Student student : students) {
+            Attendence a = new Attendence();
+            a.setLession(lession);
+            a.setStudent(student);
+            a.setDescription(req.getParameter("description" + student.getId()));
+            a.setPresent(req.getParameter("present" + student.getId()).equals("yes"));
+            atts.add(a);
+        }
+        db.takeAttendances(leid, atts);
+        resp.sendRedirect("att?id=" + leid);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp, Account account) throws ServletException, IOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int leid = Integer.parseInt(req.getParameter("id"));
+        LessionDBContext db = new LessionDBContext();
+        ArrayList<Attendence> atts = db.getAttendencesByLession(leid);
+        req.setAttribute("atts", atts);
+        req.getRequestDispatcher("../view/lecturer/att.jsp").forward(req, resp);
+
     }
 
 }
